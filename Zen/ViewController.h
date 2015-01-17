96dd4a8e88b01d3e1486d8804dd4482d57500dac
Zuhayeer Musa <zuhaz3@gmail.com>
2015-01-16 17:59:33 -0800

    initalized interactive notifications

---
+++ Zen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zen
+//
+//  Created by Zuhayeer Musa on 1/16/15.
+//  Copyright (c) 2015 Zuhayeer Musa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Zen/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Zen
+//
+//  Created by Zuhayeer Musa on 1/16/15.
+//  Copyright (c) 2015 Zuhayeer Musa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
